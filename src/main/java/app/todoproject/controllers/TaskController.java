7d3485bc7e6231874329ef09f429aafa20b8c21e
conceicaod <60795870+conceicaod@users.noreply.github.com>
2022-07-22 12:56:16 -0400

    Added more controls

--- src/main/java/app/todoproject/controllers/TaskController.java
+++ src/main/java/app/todoproject/controllers/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -33,6 +34,13 @@ public class TaskController {
     @RequestMapping(value={"/", "/home", "/index"})
     public String loadHomePage(Model model){
         List<Todo> allCurrentTasks = todoService.getAllTasks();
+        int count = 0;
+        for(Todo task : allCurrentTasks){
+            if(!task.isComplete()){
+                count++;
+            }
+        }
+        model.addAttribute("inCompleteCount", count);
         model.addAttribute("tasks", allCurrentTasks);
         return "index";
     }
@@ -48,4 +56,50 @@ public class TaskController {
         todoService.removeTask(id);
         return "redirect:/";
     }
+
+    @RequestMapping("/showIncompleteTasks")
+    public String loadIncompleteTasks(Model model){
+        List<Todo> allCurrentTasks = todoService.getAllTasks();
+        List<Todo> incompleteTasks = new ArrayList<>();
+        int count = 0;
+        for(Todo task : allCurrentTasks){
+            if(!task.isComplete()){
+                incompleteTasks.add(task);
+                count++;
+            }
+        }
+        model.addAttribute("inCompleteCount", count);
+
+        model.addAttribute("tasks", incompleteTasks);
+        return "index";
+    }
+    @RequestMapping("/showCompleteTasks")
+    public String loadCompleteTasks(Model model){
+        List<Todo> allCurrentTasks = todoService.getAllTasks();
+        List<Todo> completeTasks = new ArrayList<>();
+        int count  = 0;
+        for(Todo task : allCurrentTasks){
+            if(task.isComplete()){
+                completeTasks.add(task);
+            }
+            else{
+                count++;
+            }
+        }
+        model.addAttribute("inCompleteCount", count);
+        model.addAttribute("tasks", completeTasks);
+        return "index";
+    }
+
+    @RequestMapping("/clearCompletedTasks")
+    public String clearComplete(Model model){
+        List<Todo> allCurrentTasks = todoService.getAllTasks();
+        for(Todo task : allCurrentTasks){
+            if(task.isComplete()){
+              todoService.removeTask(task.getId());
+            }
+        }
+        return "redirect:/";
+    }
+
 }
